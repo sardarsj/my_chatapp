d86b22e3048e57ffe6c13cdbdbc0e50c41448bd8
sardarsj <sardarsjsingh08@gmail.com>
2024-05-15 17:19:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './detail.css'
+
+const Detail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Detail
